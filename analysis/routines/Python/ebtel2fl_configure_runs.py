e812ac535e2eb5b42fd95aed2055805e101273ce
Will Barnes <wtb2@rice.edu>
2015-02-25 19:33:24 -0600

    Changed bounds on DEM plots in compare_dem script; added 'solver' tag to the config and data filenames that are printed by the 'configure_runs' script; might be better to add subdirectories for solvers rather than additional tags

--- analysis/routines/Python/ebtel2fl_configure_runs.py
+++ analysis/routines/Python/ebtel2fl_configure_runs.py
@@ -2,7 +2,7 @@
 #Author: Will Barnes
 #Date: 17 February 2014
 
-#Description: Configure EBTEL-2fluid runs 
+#Description: Configure EBTEL-2fluid runs
 
 #Set up root directory extension
 root = '/data/datadrive2/EBTEL-2fluid_runs/'
@@ -18,18 +18,18 @@ import numpy as np
 def config_start_end_time(t_wait, t_total, t_pulse):
     #Calculate the number of pulses
     N = int(np.ceil(t_total/(t_pulse + t_wait)))
-    
+
     #Create the needed arrays
     t_start_array = np.empty([N])
     t_end_array = np.empty([N])
-    
+
     #Create start and end time arrays
     for i in range(N):
         t_start_array[i] = i*(t_pulse + t_wait)
         t_end_array[i] = t_start_array[i] + t_pulse
-    
+
     return {'num_events':N,'t_start_array':t_start_array,'t_end_array':t_end_array}
-    
+
 def power_law_dist(x0, x1, x, alpha):
     return ((x1**(alpha+1) - x0**(alpha+1))*x + x0**(alpha+1))**(1/(alpha+1))
 
@@ -46,7 +46,7 @@ T_wait = np.arange(250,5250,250)
 t_pulse = 200.0
 
 #Configure static parameters
-config_dict = {'usage_option':'dem','rad_option':'rk','dem_option':'new','heat_flux_option':'limited','solver':'rka4','ic_mode':'st_eq'}
+config_dict = {'usage_option':'dem','rad_option':'rk','dem_option':'new','heat_flux_option':'limited','solver':'euler','ic_mode':'st_eq'}
 config_dict['total_time'] = 80000
 config_dict['tau'] = 1.0
 config_dict['rka_error'] = 1.0e-6
@@ -65,7 +65,7 @@ config_dict['std_t_start'] = 1000
 
 #Configure directory-level parameters
 config_dict['heat_species'] = 'ion'
-config_dict['amp_switch'] = 'power_law'
+config_dict['amp_switch'] = 'uniform'
 config_dict['alpha'] = -1.5
 config_dict['loop_length'] = 20.0
 config_dict['amp0'] = Q0/(config_dict['loop_length']*1.0e+8*Ah*t_pulse) #lower bound on nanoflare volumetric heating rate
@@ -77,27 +77,26 @@ if config_dict['amp_switch'] == 'uniform':
     top_dir = top_dir + 'alpha' + config_dict['amp_switch'] + '/'
 else:
     top_dir = top_dir + 'alpha' + str(-1*config_dict['alpha']) + '/'
-config_dir = root + 'analysis/routines/Python/' + top_dir + 'config/'
-data_dir = root + 'analysis/routines/Python/' + top_dir + 'data/'
+config_dir = root + top_dir + 'config/'
+data_dir = root + top_dir + 'data/'
 
 #Loop over different values of time between successive nanoflares
 for i in range(len(T_wait)):
-    
+
     #Calculate the start and end time arrays and the number of events
     heat_times = config_start_end_time(T_wait[i], config_dict['total_time'], t_pulse)
     config_dict['num_events'] = heat_times['num_events']
     config_dict['start_time_array'] = heat_times['t_start_array']
     config_dict['end_time_array'] = heat_times['t_end_array']
-    
+
     #Set the uniform peak nanoflare energy (for triangular pulses)
     config_dict['h_nano'] = 2*Hn*config_dict['total_time']/(config_dict['num_events']*t_pulse)
-    
+
     #Concatenate the filename
-    fn = 'ebtel2fl_L' + str(config_dict['loop_length']) + '_tn' + str(T_wait[i]) + '_tpulse' + str(t_pulse)
-    
+    fn = 'ebtel2fl_L' + str(config_dict['loop_length']) + '_tn' + str(T_wait[i]) + '_tpulse' + str(t_pulse) + '_' + config_dict['solver']
+
     #Set the ouput filename
-    config_dict['output_file'] = data_dir + fn  
-    
+    config_dict['output_file'] = data_dir + fn
+
     #Print the config file (use same filename as output with _config)
     ew.print_xml_config(config_dict,config_file=config_dir+fn+'.xml')
-
