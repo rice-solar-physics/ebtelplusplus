4fc602eac19a9ad60bb0b554a51b72933f990f39
Will Barnes <wtb2@rice.edu>
2015-03-15 23:42:01 -0500

    Added function to calculate hot shoulder and evaluate shoulder strength through integration of shoulder versus total integration for DEM two orders of magnitude below max(DEM) value

--- analysis/routines/Python/ebtel2fl_dem_analysis.py
+++ analysis/routines/Python/ebtel2fl_dem_analysis.py
@@ -7,7 +7,6 @@ import matplotlib
 matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
-import argparse
 
 def dem_shoulder_compare(temp,dem):
     """Compute integral of hot and cold shoulder and calculate ratio to provide quantitative measure of hot DEM component.
@@ -28,7 +27,56 @@ def dem_shoulder_compare(temp,dem):
     #If the DEM value is +/-Inf, find the closest value that =! +/-Inf
     dem_bound = 1.0e-2*10**(dem_max)
     dem_bound = np.log10(dem_bound)
-
+    
+    #Create cool and hot DEM and temperature arrays
+    dem_hot = dem[i_dem_max:-1]
+    temp_hot = temp[i_dem_max:-1]
+    dem_cool = dem[0:i_dem_max]
+    temp_cool = dem[0:i_dem_max]
+    
+    #Find the indices for hot and cool bounds
+    i_dem_hot = np.where(dem_hot > dem_bound)[0][-1]
+    i_dem_cool = np.where(dem_cool > dem_bound)[0][0]
+    
+    #Make sure that the indices do not correspond to 'Inf' DEM values
+    found_cool_bound = False
+    found_hot_bound = False
+    
+    while found_cool_bound == False or found_hot_bound == False:
+        if found_cool_bound == False:
+            if np.isinf(dem_cool[i_dem_cool]):
+                i_dem_cool = i_dem_cool + 1
+            else:
+                temp_bound_cool = temp_cool[i_dem_cool]
+                found_cool_bound = True
+                
+        if found_hot_bound == False:
+            if np.isinf(dem_hot[i_dem_hot]):
+                i_dem_hot = i_dem_hot - 1
+            else:
+                temp_bound_hot = temp_hot[i_dem_hot]
+                found_hot_bound = True
+                
+                
+    #Now that we have the bounds, do the integration
+    #Hot shoulder 
+    hot_shoulder = np.trapz(dem[i_dem_max:(i_dem_max+i_dem_hot)],x=temp[i_dem_max:(i_dem_max+i_dem_hot)])
+    #Total
+    total_shoulder = np.trapz(dem[i_dem_cool:(i_dem_max+i_dem_hot)],x=temp[i_dem_cool:(i_dem_max+i_dem_hot)])
+    #Compute the ratio
+    hot_shoulder_strength = hot_shoulder/total_shoulder
+    
+    print "Hot shoulder strength = ",hot_shoulder_strength
+    
+    #DEBUG--do some quick plotting
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.plot(temp,dem,'k')
+    ax.plot([temp[0],temp[-1]],[dem_bound,dem_bound],'--r')
+    ax.plot([temp[i_dem_cool],temp[i_dem_max],temp[i_dem_max+i_dem_hot]],[dem[i_dem_cool],dem[i_dem_max],dem[i_dem_max+i_dem_hot]],'bo')
+    plt.show()
+    
+    return hot_shoulder_strength
 
 
 def plot_ebtel_dem_compare(species,alpha,L,t_pulse,solver,**kwargs):
