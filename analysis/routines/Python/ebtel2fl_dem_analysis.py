Fixes to naming conventions in fit calculation

analysis/routines/Python/ebtel2fl_dem_analysis.py
@@ -95,15 +95,15 @@ def dem_shoulder_compare_fit(temp,dem,delta_hot,delta_cool):
 
     #Calculate the cool and hot bounds (in DEM and temperature)
     #Cool shoulder
-    temp_cool = temp_dem_max - delta_cool
+    temp_cool_bound = temp_dem_max - delta_cool
     #Hot shoulder
-    dem_hot = dem_max - delta_hot
+    dem_hot_bound = dem_max - delta_hot
     
     #Check if our bounds are valid for these temp and dem arrays
     #If they are valid, calculate the hotward and coolward slopes
     #Cool branch
-    if temp_cool <= temp[inf_index_cool]:
-        print "Cool bound out of range. T_cool = ",temp_cool," < T_inf_cool = ",temp[inf_index_cool]
+    if temp_cool_bound <= temp[inf_index_cool]:
+        print "Cool bound out of range. T_cool = ",temp_cool_bound," < T_inf_cool = ",temp_cool[inf_index_cool]
         a_coolward = False
     else:
         #Interpolate over the cool branch
@@ -111,12 +111,12 @@ def dem_shoulder_compare_fit(temp,dem,delta_hot,delta_cool):
         temp_cool_new = np.linspace(temp_cool[inf_index_cool],temp_cool[-1],1000)
         dem_cool_new = f(temp_cool_new)
         #Find the more accurate index of the cool bound
-        i_bound_cool = np.where(temp_cool_new > temp_cool)[0][0] - 1
+        i_bound_cool = np.where(temp_cool_new > temp_cool_bound)[0][0] - 1
         #Calculate the coolward slope
         a_coolward = (dem_cool_new[-1] - dem_cool_new[i_bound_cool])/(temp_cool_new[-1] - temp_cool_new[i_bound_cool])
         
     #Hot branch    
-    if dem_hot <= dem_hot[inf_index_hot]:
+    if dem_hot_bound <= dem_hot[inf_index_hot]:
         print "Hot bound out of range."
         a_hotward = False
     else:
@@ -125,7 +125,7 @@ def dem_shoulder_compare_fit(temp,dem,delta_hot,delta_cool):
         temp_hot_new = np.linspace(temp_hot[0],temp_hot[inf_index_hot],1000)
         dem_hot_new = f(temp_hot_new)
         #Find the more accurate index of the hot bound
-        i_bound_hot = np.where(dem_hot_new > dem_hot)[0][0] - 1
+        i_bound_hot = np.where(dem_hot_new < dem_hot_bound)[0][0] - 1
         #Calculate the hotward slope
         a_hotward = (dem_hot_new[i_bound_hot] - dem_hot_new[0])/(temp_hot_new[i_bound_hot] - temp_hot_new[0])
         