9ef07a254f117fcc536830863f0488052307ca06
Will Barnes <wtb2@rice.edu>
2015-05-13 18:52:25 -0500

    added function to calculate EM slopes for large list of EM values (MC) or only over varying Tn values; added function to plot EM curves for varying Tn--need to add function to plot single EM curve that can show errorbars

--- analysis/routines/Python/ebtel2fl_dem.py
+++ analysis/routines/Python/ebtel2fl_dem.py
@@ -6,6 +6,8 @@
 #Import needed modules
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
 
 class DEMAnalyzer(object):
     
@@ -39,8 +41,16 @@ class DEMAnalyzer(object):
             self.mc = kwargs['mc']
         else:
             self.mc = False
-        #process data
-        self.process_raw()
+        if 'delta_cool' in kwargs:
+            self.delta_cool = kwargs['delta_cool']
+        else:
+            self.delta_cool = -0.45
+        if 'delta_hot' in kwargs:
+            self.delta_hot = kwargs['delta_hot']
+        else:
+            self.delta_hot = -2.0
+        #set static variables
+        self.em_cutoff = 23.0
             
             
     def process_raw(self,**kwargs):
@@ -65,6 +75,108 @@ class DEMAnalyzer(object):
                         pass
                 self.temp_em.append(temp_em)
                 self.em.append(em)
-                    
-                    
-    
+                
+                
+    def many_slopes(self,**kwargs):
+        self.a_cool = []
+        self.a_hot = []
+        for i in range(len(self.Tn)):
+            if self.mc is False:
+                ac,ah = self.slope(self.temp_em[i],self.em[i])
+                self.a_cool.append(ac),self.a_hot.append(ah)
+            else:
+                acl = []
+                ahl = []
+                for j in range(self.mc):
+                    ac,ah = self.slope(self.temp_em[i][j],self.em[i][j])
+                    acl.append(ac),ahl.append(ah)
+                self.a_cool.append(acl),self.a_hot.append(ahl)
+
+
+    def slope(self,temp,dem,**kwargs):
+        #Calculate bounds
+        dict_bounds = self.bounds(temp,dem)
+        
+        #Function for linear fit
+        def linear_fit(x,a,b):
+            return a*x + b
+            
+        #Check if inside interpolated array and calculate slope
+        #cool
+        if np.size(dict_bounds['bound_cool']) == 0:
+            print "Cool bound out of range. T_cool_bound = ",temp[np.argmax(dem)] + self.delta_cool," < T_cool(0) = ",dict_bounds['temp_cool'][0]
+            a_coolward = False
+        else:
+            bound_cool = dict_bounds['bound_cool'][0][-1] + 1
+            pars_cool,covar = curve_fit(linear_fit,dict_bounds['temp_cool'][bound_cool:-1],dict_bounds['dem_cool'][bound_cool:-1])
+            a_coolward = pars_cool[0]
+        #hot
+        if np.size(dict_bounds['bound_hot']) == 0:
+            print "Hot bound out of range. DEM_hot_bound = ",dem[np.argmax(dem)] + self.delta_hot," < DEM_hot(end) = ",dict_bounds['dem_hot'][-1]
+            a_hotward = False
+        else:
+            bound_hot = dict_bounds['bound_hot'][0][0] - 1
+            pars_hot,covar = curve_fit(linear_fit,dict_bounds['temp_hot'][0:bound_hot],dict_bounds['dem_hot'][0:bound_hot])
+            a_hotward = pars_hot[0]
+            
+        return a_coolward,a_hotward
+        
+        
+    def integrate(self,temp,dem,**kwargs):
+        #Find the corresponding temperature bounds
+        dict_bounds = self.bounds(temp,dem)
+
+        #First check if the bounds are inside of our interpolated array
+        if np.size(dict_bounds['bound_cool']) == 0 or np.size(dict_bounds['bound_hot']) == 0:
+            print "Cool and/or hot bound(s) out of range. Skipping integration for these bounds."
+            hot_shoulder_strength = False
+        else:
+            #Refine the arrays we will integrate over
+            #Temprature
+            temp_hot = dict_bounds['temp_hot'][0:(dict_bounds['bound_hot'][0][0] - 1)]
+            temp_cool = dict_bounds['temp_cool'][(dict_bounds['bound_cool'][0][-1] + 1):-1]
+            #DEM (EM)
+            dem_hot = dict_bounds['dem_hot'][0:(dict_bounds['bound_hot'][0][0] - 1)]
+            dem_cool = dict_bounds['dem_cool'][(dict_bounds['bound_cool'][0][-1] + 1):-1]
+            #Do the integration
+            #Hot shoulder
+            hot_shoulder = np.trapz(dem_hot,x=temp_hot)
+            #Total
+            total_shoulder = np.trapz(np.concatenate([dem_cool[0:-1],dem_hot]),x=np.concatenate([temp_cool[0:-1],temp_hot]))
+            #Compute the ratio
+            hot_shoulder_strength = hot_shoulder/total_shoulder
+
+        return hot_shoulder_strength
+        
+        
+    def bounds(self,temp,dem,**kwargs):
+        #Find peak parameters
+        dem_max = np.max(dem)
+        i_dem_max = np.argmax(dem)
+        temp_dem_max = temp[i_dem_max]
+
+        #Create cool and hot DEM and temperature arrays
+        dem_hot = dem[i_dem_max:-1]
+        temp_hot = temp[i_dem_max:-1]
+        dem_cool = dem[0:i_dem_max]
+        temp_cool = temp[0:i_dem_max]
+
+        #Find the dem index where dem->inf (or less than the cutoff)
+        inf_index_hot = np.where(dem_hot > self.em_cutoff)[0][-1]
+        inf_index_cool = np.where(dem_cool > self.em_cutoff)[0][0]
+
+        #Calculate the cool and hot bounds (in DEM and temperature)
+        temp_cool_bound = temp_dem_max + self.delta_cool
+        dem_hot_bound = dem_max + self.delta_hot
+
+        #Interpolate and find accurate bound index
+        temp_cool_new = np.linspace(temp_cool[inf_index_cool],temp_cool[-1],1000)
+        dem_cool_new = np.interp(temp_cool_new,temp_cool[inf_index_cool:-1],dem_cool[inf_index_cool:-1])
+        i_bound_cool = np.where(temp_cool_new < temp_cool_bound)
+        temp_hot_new = np.linspace(temp_hot[0],temp_hot[inf_index_hot],1000)
+        dem_hot_new = np.interp(temp_hot_new,temp_hot[0:inf_index_hot],dem_hot[0:inf_index_hot])
+        i_bound_hot = np.where(dem_hot_new < dem_hot_bound)
+
+        #Return the list of indices and interpolated DEM and temperature arrays
+        return {'bound_cool':i_bound_cool,'bound_hot':i_bound_hot,'temp_cool':temp_cool_new,'temp_hot':temp_hot_new,'dem_cool':dem_cool_new,'dem_hot':dem_hot_new}
+        
--- analysis/routines/Python/ebtel2fl_plot.py
+++ analysis/routines/Python/ebtel2fl_plot.py
@@ -12,8 +12,12 @@ from scipy.optimize import curve_fit
 
 class Plotter(object):
     
-    def __init__(self,parent_dir,**kwargs):
-        self.parent_dir = parent_dir
+    def __init__(self,**kwargs):
+        #configure keyword arguments
+        if 'parent_dir' not in kwargs:
+            print "Warning: Parent directory not specified. You will not be able to load variables into class instance."
+        else:
+            self.parent_dir = kwargs['parent_dir']
         if 'alpha' in kwargs:
             self.alpha = kwargs['alpha']
         if 'tpulse' in kwargs:
@@ -27,8 +31,15 @@ class Plotter(object):
         if 'Tn' in kwargs and 'run' in kwargs:
             self.Tn,self.run = kwargs['Tn'],kwargs['run']
             self.load_variables()
-        if 'data_file' in kwargs and kwargs is False:
+        elif 'data_file' in kwargs:
             self.load_variables(data_file=kwargs['data_file'])
+        #configure static parameters
+        self.fs = 18.0
+        self.figsize = (10,10)
+        self.linestyles = ('-','--','-.',':')
+        self.format = 'eps'
+        self.dpi = '1000'
+            
             
     def load_variables(self,**kwargs):
         if 'data_file' in kwargs:
@@ -46,3 +57,36 @@ class Plotter(object):
         self.temp_apex_i = data[:,8]
         self.dens_apex = data[:,9]
         self.heat = data[:,15]
+        
+        
+    def plot_em_curves(self,temp_list,em_list,**kwargs):
+        #spacing between tn curves (artificial)
+        delta_em = 0.2
+        
+        #set up figure
+        fig = plt.figure(figsize=self.figsize)
+        ax = fig.gca()
+        
+        #print lines
+        for i in range(len(em_list)):
+            if len(np.shape(np.array(em_list[i]))) > 1:
+                mean_em = np.mean(em_list[i],axis=0)
+                std_em = np.std(em_list[i],axis=0)
+                mean_temp = np.mean(temp_list[i],axis=0)
+                ax.plot(mean_temp,mean_em+i*delta_em,linestyle=self.linestyles[i%len(self.linestyles)],color='blue')
+            else:
+                ax.plot(temp_list[i],em_list[i]+i*delta_em,linestyle=self.linestyles[i%len(self.linestyles)],color='blue')
+        
+        #set labels
+        ax.set_title(r'EBTEL Two-fluid EM, $\alpha$ = '+str(self.alpha),fontsize=self.fs)
+        ax.set_xlabel(r'$\log T$ (K)',fontsize=self.fs)
+        ax.set_ylabel(r'$\log$EM (cm$^{-5}$)',fontsize=self.fs)
+        ax.set_xlim([5.5,7.5])
+        ax.set_ylim([27,33])
+        
+        #save or show the figure
+        if 'print_fig_filename' in kwargs:
+            plt.savefig(kwargs['print_fig_filename']+'.'+self.format,format=self.format,dpi=self.dpi)
+        else:
+            plt.show()
+                
