c9e3bb798dcfd0f223592038980c6a7de635fe14
Will Barnes <wtb2@rice.edu>
2015-05-13 15:14:24 -0500

    added function to process DEM data for either normal or MC-type simulations

--- analysis/routines/Python/ebtel2fl_dem.py
+++ analysis/routines/Python/ebtel2fl_dem.py
@@ -9,11 +9,15 @@ import matplotlib.pyplot as plt
 
 class DEMAnalyzer(object):
     
-    def __init__(self,alpha,loop_length,tpulse,solver,**kwargs):
+    def __init__(self,root_dir,alpha,loop_length,tpulse,solver,**kwargs):
+        self.root_dir = root_dir
         self.alpha = alpha
         self.loop_length = loop_length
         self.tpulse = tpulse
         self.solver = solver
+        child_path = self.root_dir+self.species+'_heating_runs/alpha'+str(self.alpha)+'/data/'
+        self.file_path = 'ebtel2fl_L'+str(self.loop_length)+'_tn%d_tpulse'+str(self.tpulse)+'_'+self.solver
+        self.root_path = child_path + self.file_path
         if 'Tna' in kwargs:
             self.Tna = kwargs['Tna']
         else:
@@ -31,3 +35,24 @@ class DEMAnalyzer(object):
             self.mc = kwargs['mc']
         else:
             self.mc = False
+            
+            
+    def process_raw(self,**kwargs):
+        self.em = []
+        self.temp_em = []
+        for i in range(len(self.Tn)):
+            tn_path = self.root_path%self.Tn[i]
+            if self.mc is False:
+                temp = np.loadtxt(tn_path+'_dem.txt')
+                self.temp_em.append(temp[:,0])
+                self.em.append(temp[:,4])
+            else:
+                em = []
+                temp_em = []
+                for j in range(self.mc):
+                    temp = np.loadtxt(tn_path+'/'+self.file_path%self.Tn[i]+'_'+str(j)+'_dem.txt')
+                    temp_em.append(temp[:,0])
+                    em.append(temp[:,4])
+                self.temp_em.append(temp_em)
+                self.em.append(em)
+                    
