0b8a614f2803e5eb549a0059625f5af772b9a75d
Will Barnes <wtb2@rice.edu>
2016-09-07 23:28:12 -0500

    indentation

--- source/main.cpp
+++ source/main.cpp
@@ -21,10 +21,10 @@ int main(int argc, char *argv[])
   namespace po = boost::program_options;
   po::options_description description("ebtel++ A code for efficiently computing the evolution of dynamically-heated coronal loops. Based on the model of Klimchuk et al. (2008).");
   description.add_options()
-  	("help,h","The help message")
+    ("help,h","The help message")
     ("quiet,q",po::bool_switch()->default_value(false),"Suppress output.")
     ("ebtel_config,c",po::value<std::string>()->default_value("config/ebtel.example.cfg.xml"),"Configuration file for EBTEL.")
-  	("rad_config,r",po::value<std::string>()->default_value("config/radiation.example.cfg.xml"),"Configuration file for radiation class");
+    ("rad_config,r",po::value<std::string>()->default_value("config/radiation.example.cfg.xml"),"Configuration file for radiation class");
   po::variables_map vm;
   po::store(po::command_line_parser(argc,argv).options(description).run(), vm);
   if(vm.count("help"))
