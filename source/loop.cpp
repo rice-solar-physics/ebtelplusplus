1722303f5cf40125fbd16f8c385ea717b6905881
Will Barnes <wtb2@rice.edu>
2016-09-11 15:22:55 -0500

    fully implemented initial conditions calculation

--- source/loop.cpp
+++ source/loop.cpp
@@ -5,9 +5,12 @@ Loop object that will hold all information about the loop and be evolved in time
 
 #include "loop.h"
 
-Loop::Loop(char *ebtel_config, PRADIATION radiation_model)
+Loop::Loop(char *ebtel_config, char *rad_config)
 {
   tinyxml2::XMLDocument doc;
+  tinyxml2::XMLElement *root;
+  tinyxml2::XMLElement *heating;
+  double helium_to_hydrogen_ratio;
 
   //Open file
   tinyxml2::XMLError load_ok = doc.LoadFile(ebtel_config);
@@ -17,34 +20,210 @@ Loop::Loop(char *ebtel_config, PRADIATION radiation_model)
   	//TODO: Exit or break out from here
   }
   //Parse file and read into data structure
-  tinyxml2::XMLElement *root = doc.FirstChildElement();
+  root = doc.FirstChildElement();
   //Numeric parameters
   parameters.total_time = std::stod(get_element_text(root,"total_time"));
   parameters.tau = std::stod(get_element_text(root,"tau"));
   parameters.loop_length = std::stod(get_element_text(root,"loop_length"));
   parameters.rka_error = std::stod(get_element_text(root,"loop_length"));
+  parameters.saturation_limit = std::stod(get_element_text(root,"saturation_limit"));
+  parameters.c1_cond0 = std::stod(get_element_text(root,"c1_cond0"));
+  parameters.c1_rad0 = std::stod(get_element_text(root,"c1_rad0"));
+  helium_to_hydrogen_ratio = std::stod(get_element_text(root,"helium_to_hydrogen_ratio"));
   //Boolean parameters
   parameters.use_c1_loss_correction = string2bool(get_element_text(root,"use_c1_loss_correction"));
   parameters.use_c1_grav_correction = string2bool(get_element_text(root,"use_c1_grav_correction"));
   parameters.use_power_law_radiative_losses = string2bool(get_element_text(root,"use_power_law_radiative_losses"));
   parameters.use_spitzer_conductivity = string2bool(get_element_text(root,"use_spitzer_conductivity"));
+  parameters.calculate_dem = string2bool(get_element_text(root,"calculate_dem"));
   //String parameters
   parameters.solver = get_element_text(root,"solver");
+  parameters.output_filename = get_element_text(root,"output_filename");
+
+  //Initialize radiation model object
+  if(parameters.use_power_law_radiative_losses)
+  {
+    radiation_model = new CRadiation();
+  }
+  else
+  {
+    radiation_model = new CRadiation(rad_config,false);
+  }
 
   //Initialize heating object
+  heating = get_element(root,"heating");
+  heater = new Heater(heating);
+
   //Initialize DEM object
-  //TODO: implement after everything else is in place
+  //TODO: implement after everything else is in place; only instantiate if we want to do that calculation
 
   doc.Clear();
+
+  // Calculate needed He abundance corrections
+  CalculateAbundanceCorrection(helium_to_hydrogen_ratio);
+
+  //Estimate results array length
+  N = int(ceil(parameters.total_time/parameters.tau));
+  //Reserve memory for results
+  results.time.resize(N);
+  results.heat.resize(N);
+  results.pressure_e.resize(N);
+  results.pressure_i.resize(N);
+  results.temperature_e.resize(N);
+  results.temperature_i.resize(N);
+  results.density.resize(N);
 }
 
 Loop::~Loop(void)
 {
   //Destructor--free some stuff here
+  delete heater;
+  delete radiation_model;
 }
 
 void Loop::CalculateInitialConditions(void)
 {
-  //Calculate initial conditions
-  //Set initial state
+  int i = 0;
+  int i_max = 100;
+  double tol = 1e-2;
+  double temperature_old = LARGEST_DOUBLE;
+  double density_old = LARGEST_DOUBLE;
+  double temperature,density;
+  double radiative_loss;
+  double error_temperature,error_density;
+  double c1 = 2.0;
+  double c2 = CalculateC2();
+  double heat = heater->Get_Heating(0.0);
+
+  while(i<i_max)
+  {
+    if(i > 0)
+    {
+      c1 = CalculateC1(temperature_old, temperature_old, density_old);
+    }
+    temperature = c2*pow(3.5*c1/(1.0 + c1)*pow(parameters.loop_length,2)*heat/(SPITZER_ELECTRON_CONDUCTIVITY + SPITZER_ION_CONDUCTIVITY),2.0/7.0);
+    radiative_loss = radiation_model->GetPowerLawRad(log10(temperature));
+    density = sqrt(heat/(radiative_loss*(1.0 + c1)));
+    error_temperature = fabs(temperature - temperature_old)/temperature;
+    error_density = fabs(density - density_old)/density;
+    if(fmax(error_density,error_temperature) < tol)
+    {
+      break;
+    }
+    i++;
+    temperature_old = temperature;
+    density_old = density;
+  }
+
+  // Set current state in order pressure_e, pressure_i, density
+  state.resize(3);
+  state[0] = BOLTZMANN_CONSTANT*density*temperature;
+  state[1] = parameters.boltzmann_correction*BOLTZMANN_CONSTANT*density*temperature;
+  state[2] = density;
+
+  //Save the results
+  SaveResults(0,0.0);
+}
+
+void Loop::EvolveLoop(void)
+{
+  int i=1;
+  double time = parameters.tau;
+  double heat;
+
+  while(time<parameters.total_time)
+  {
+    // Get current heating
+    heat = heater->Get_Heating(time);
+    // Solve Equations--update state
+    // Save results
+    SaveResults(i,time);
+    //Update time and counter
+    time += parameters.tau;
+    i++;
+  }
+}
+
+void Loop::PrintToFile(void)
+{
+  std::ofstream f;
+  f.open(parameters.output_filename);
+  for(int i=0;i<results.time.size();i++)
+  {
+    f << results.time[i] << "\t" << results.temperature_e[i] << "\t" << results.temperature_i[i] << "\t" << results.density[i] << "\t" << results.pressure_e[i] << "\t" << results.pressure_i[i] << "\t" << results.heat[i] << "\n";
+  }
+  f.close();
+}
+
+void Loop::SaveResults(int i,double time)
+{
+  // calculate parameters
+  double temperature_e = state[0]/(BOLTZMANN_CONSTANT*state[2]);
+  double temperature_i =  state[1]/(parameters.boltzmann_correction*BOLTZMANN_CONSTANT*state[2]);
+  double heat = heater->Get_Heating(time);
+  // Save results to results structure
+  results.time[i] = time;
+  results.heat[i] = heat;
+  results.temperature_e[i] = temperature_e;
+  results.temperature_i[i] = temperature_i;
+  results.pressure_e[i] = state[0];
+  results.pressure_i[i] = state[1];
+  results.density[i] = state[2];
+}
+
+double Loop::CalculateC1(double temperature_e, double temperature_i, double density)
+{
+  double c1;
+  double c1_eqm0 = 2.0;
+  double c2 = CalculateC2();
+  double grav_correction = 1.0;
+  double loss_correction = 1.0;
+  double scale_height = CalculateScaleHeight(temperature_e,temperature_i);
+  double radiative_loss = radiation_model->GetPowerLawRad(log10(temperature_e));
+  double density_eqm_2,density_ratio;
+
+  if(parameters.use_c1_grav_correction)
+  {
+    grav_correction = exp(4.0*sin(_PI_/5.0)*parameters.loop_length/(_PI_*scale_height));
+  }
+  if(parameters.use_c1_loss_correction)
+  {
+    loss_correction = 1.95e-18*pow(temperature_e,-2.0/3.0)/radiative_loss;
+  }
+
+  density_eqm_2 = (SPITZER_ELECTRON_CONDUCTIVITY+SPITZER_ION_CONDUCTIVITY)*pow(temperature_e/c2,3.5)/(3.5*pow(parameters.loop_length,2)*c1_eqm0*loss_correction*grav_correction);
+  density_ratio = pow(density,2)/density_eqm_2;
+
+  if(density_ratio<1.0)
+  {
+    c1 = (2.0*c1_eqm0 + parameters.c1_cond0*(1.0/density_ratio - 1.0))/(1.0 + 1.0/density_ratio);
+  }
+  else
+  {
+    c1 = (2.0*c1_eqm0 + parameters.c1_rad0*(density_ratio - 1.0))/(1.0 + density_ratio);
+  }
+
+  return c1;
+}
+
+double Loop::CalculateC2(void)
+{
+  return 0.9;
+}
+
+double Loop::CalculateC3(void)
+{
+  return 0.6;
+}
+
+double Loop::CalculateScaleHeight(double temperature_e,double temperature_i)
+{
+  return BOLTZMANN_CONSTANT*(temperature_e + parameters.boltzmann_correction*temperature_i)/(parameters.ion_mass_correction*PROTON_MASS)/SOLAR_SURFACE_GRAVITY;
+}
+
+void Loop::CalculateAbundanceCorrection(double helium_to_hydrogen_ratio)
+{
+  double z_avg = (1.0 + 2.0*helium_to_hydrogen_ratio)/(1.0 + helium_to_hydrogen_ratio);
+  parameters.boltzmann_correction = (1.0 + 1.0/z_avg)/2.0;
+  parameters.ion_mass_correction = (1.0 + 4.0*helium_to_hydrogen_ratio)/(2.0 + 3.0*helium_to_hydrogen_ratio)*2.0*parameters.boltzmann_correction;
 }
--- source/loop.h
+++ source/loop.h
@@ -7,8 +7,10 @@ Loop class definition
 #define LOOP_H
 
 #include "helper.h"
+#include "heater.h"
 #include "../Radiation_Model/source/radiation.h"
 #include "../rsp_toolkit/source/file.h"
+#include "../rsp_toolkit/source/constants.h"
 
 // Loop class
 //
@@ -19,8 +21,55 @@ Loop class definition
 class Loop {
 private:
 
-  // Parameter structure
+  /* Parameter structure*/
   Parameters parameters;
+
+  /* Results structure */
+  Results results;
+
+  /* Instance of the <Heater> object */
+  HEATER heater;
+
+  /* Instance of the <CRadiation> object */
+  PRADIATION radiation_model;
+
+  /* Current state of the system */
+  std::vector<double> state;
+
+  /* Number of entries in results */
+  size_t N;
+
+  // Save results to structure
+  //
+  void SaveResults(int i, double time);
+
+  // Calculate c1
+  // @temperature_e electron temperature (in K)
+  // @temperature_i ion temperature (in K)
+  // @density number density (in cm^-3)
+  //
+  // Calculate the c1 parameter, the ratio between the
+  // transition and coronal radiative losses
+  //
+  // @return c1 parameter
+  //
+  double CalculateC1(double temperature_e,double temperature_i,double density);
+
+  // Calculate c2
+  //
+  double CalculateC2(void);
+
+  // Calculate c3
+  //
+  double CalculateC3(void);
+
+  // Calculate temperature scale height
+  //
+  double CalculateScaleHeight(double temperature_e,double temperature_i);
+
+  // Calculate correction for He abundance
+  //
+  void CalculateAbundanceCorrection(double helium_to_hydrogen_ratio);
 
 public:
 
@@ -28,7 +77,7 @@ public:
   // @ebtel_config name of main configuration file
   // @radiation_model instance of <CRadiation> class
   //
-  Loop(char * ebtel_config,PRADIATION radiation_model);
+  Loop(char * ebtel_config,char * rad_config);
 
   /* Destructor */
   ~Loop(void);
@@ -36,6 +85,14 @@ public:
   // Set initial conditions
   //
   void CalculateInitialConditions(void);
+
+  // Evolve loop in time
+  //
+  void EvolveLoop(void);
+
+  // Print results to file
+  //
+  void PrintToFile(void);
 };
 
 typedef Loop* LOOP;
