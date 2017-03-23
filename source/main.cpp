e257abfdad534fc265f571b4ff334dc86230a10b
Will Barnes <wtb2@rice.edu>
2017-03-23 13:44:06 -0500

    better help messages, only allow a finite number of failures to prevent infinite loops

--- source/main.cpp
+++ source/main.cpp
@@ -22,9 +22,9 @@ int main(int argc, char *argv[])
 
   //Parse command line options with boost
   namespace po = boost::program_options;
-  po::options_description description("ebtel++ A code for efficiently computing the evolution of dynamically-heated coronal loops. Based on the model of Klimchuk et al. (2008).");
+  po::options_description description("\nebtel++\nA code for efficiently computing the evolution of dynamically-heated coronal loops. Based on the Enthalpy-based Thermal Evolution of Loops (EBTEL) model of Klimchuk et al. (2008) and Cargill et al. (2012). For more information, consult the documentation.\n\nOptional command line arguments");
   description.add_options()
-    ("help,h","The help message")
+    ("help,h","This help message")
     ("quiet,q",po::bool_switch()->default_value(false),"Suppress output.")
     ("ebtel_config,c",po::value<std::string>()->default_value("config/ebtel.example.cfg.xml"),"Configuration file for EBTEL.")
     ("rad_config,r",po::value<std::string>()->default_value("config/radiation.example.cfg.xml"),"Configuration file for radiation class");
@@ -63,10 +63,13 @@ int main(int argc, char *argv[])
   // Set up Runge-Kutta integrator
   typedef boost::numeric::odeint::runge_kutta_cash_karp54< state_type > stepper_type;
   auto controlled_stepper = boost::numeric::odeint::make_controlled(loop->parameters.adaptive_solver_error, loop->parameters.adaptive_solver_error, stepper_type());
+  
   // Integrate
   num_steps = 0;
   if(loop->parameters.use_adaptive_solver)
   {
+    // Set maximum number of allowed failures
+    int max_failures = 1000;
     // Initialize time and timestep
     double tau = loop->parameters.tau;
     double t = loop->parameters.tau;
@@ -75,12 +78,20 @@ int main(int argc, char *argv[])
     while(t<loop->parameters.total_time)
     {
       int fail = 1;
+      int num_failures = 0;
       while(fail>0)
       {
+        // Throw error if exceeded max number of failures to avoid infinite loop
+        if(num_failures>max_failures)
+        {
+          throw std::runtime_error("Adaptive solver exceeded maximum number of allowed failures.");
+        }
         old_tau = tau;
         old_t = t;
         fail = controlled_stepper.try_step(loop->CalculateDerivs,state,t,tau);
+        // Force NaNs to fail
         if(!fail) fail = obs->CheckNan(state,t,tau,old_t,old_tau);
+        num_failures++;
       }
       // Enforce thermal conduction timescale limit
       double tau_tc = 4e-10*state[2]*pow(loop->parameters.loop_length,2)*pow(std::fmax(state[3],state[4]),-2.5);
@@ -92,6 +103,7 @@ int main(int argc, char *argv[])
   }
   else
   {
+    // Constant timestep integration
     num_steps = boost::numeric::odeint::integrate_const( controlled_stepper, loop->CalculateDerivs, state, loop->parameters.tau, loop->parameters.total_time, loop->parameters.tau, obs->Observe);
   }
 
