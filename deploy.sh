97757556170435dfb22ee71dd6d8f2d20ef63fe7
Will Barnes <wtb2@rice.edu>
2017-09-21 11:22:26 -0500

    add deploy step to docs stage

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 scons
 cd docs && make && cd ..
-ghp-import -m "Build docs" -b gh-pages docs/html
-git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages
+# ghp-import -m "Build docs" -b gh-pages docs/html
+# git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages
