195ecdffcd6dc3cbda271fa48680a1e083e0dc38
Jeffrey Reep <reep@kahiku.local>
2024-07-04 11:15:12 -1000

    remove overloaded find_closest

--- source/util/misc.h
+++ source/util/misc.h
@@ -10,10 +10,9 @@
 #include <cstdlib>
 #include <vector>
 
-// Find the closest value in an array (or vector) to the input x
+// Find the closest value in a vector to the input x
 // Uses a simple search, O(N), so it's best for short arrays
-// Returns the index of the array (vector) that's closest to value x
-int find_closest(double x, double array[], int array_length);
+// Returns the index of the vector that's closest to value x
 int find_closest(double x, std::vector<double> array, int array_length);
 
 #endif
